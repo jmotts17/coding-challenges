ffcb6508ba1ce007283492ff4ef5ca3068d266e1
Josh Motta <joshua.a.motta@gmail.com>
2020-11-17 22:53:17 -0500

    init commit

---
+++ CC04_FirstLast6/src/FirstLast6App.java
@@ -0,0 +1,24 @@
+
+/*
+ * 
+ * Coding Challenge 4 - First Last 6
+ * 
+ * Given an array of ints, return true if 6 appears as either the first or last element in the array.
+ * The array will be length 1 or more.
+ * 
+ */
+
+public class FirstLast6App {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	public boolean firstLast6(int[] nums) {
+		
+		return true;
+	}
+
+}
