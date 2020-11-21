add8c772925f9af1281c4a05b6d0c803022ca7ac
Josh Motta <joshua.a.motta@gmail.com>
2020-11-20 22:32:43 -0500

    init commit

---
+++ CC07_StringBits/src/StringBitsApp.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * Coding Challenge 7 - String Bits
+ * 
+ * Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+ * 
+ */
+
+public class StringBitsApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(stringBits("Hello"));
+		
+	}
+	
+	public static String stringBits(String str) {
+		
+		return "";
+	}
+}
