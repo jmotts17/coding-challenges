ecfcae1c18d0d628f7b6708e2125a6782488c464
Josh Motta <joshua.a.motta@gmail.com>
2020-11-23 21:23:59 -0500

    Completed challenge

--- CC02_Arrowhead/src/ArrowheadApp.java
+++ CC02_Arrowhead/src/ArrowheadApp.java
@@ -13,36 +13,43 @@ import java.util.Scanner;
 public class ArrowheadApp {
 
 	public static void main(String[] args) {
-		
+
 		// Create Scanner Object
 		Scanner scanner = new Scanner(System.in);
-		
+
 		// Prompt user for arrow size
 		System.out.print("Please enter the size of the arrow [3 or more]: ");
 		int arrowSize = scanner.nextInt();
-		
+
 		// If arrow size is even, add a row for middle row
-		if(arrowSize % 2 == 0) {
+		if (arrowSize % 2 == 0) {
 			arrowSize++;
 		}
-		
+
 		// Call createArrow method
 		createArrow(arrowSize);
 	}
 	
+	/**
+	 * This method takes in an arrow size and then uses 2 nested for loops to
+	 * create an arrowhead. The first nested for loop creates the first half 
+	 * of the arrowhead. The second nested for loop creates the bottom half  
+	 * including the tip.
+	 * 
+	 * @param arrowSize
+	 */
 	public static void createArrow(int arrowSize) {
-		
-		for(int i = 0; i < arrowSize; i++) {
-			for(int j = 0; j <= i; j++) {
-				if(i <= (arrowSize / 2)) {
+		for (int i = 0; i <= arrowSize / 2; i++) {
+			for (int j = 0; j <= i; j++) {
 					System.out.print(">");
-				} else {
-					System.out.print(">");
-					j++;
-				}
+			}
+			System.out.println();
+		}
+		for(int i = arrowSize / 2; i <= arrowSize; i++) {
+			for (int j = i; j <= arrowSize; j++) {
+				System.out.print(">");
 			}
 			System.out.println();
 		}
 	}
-
 }
