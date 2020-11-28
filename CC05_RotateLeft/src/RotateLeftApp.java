2a73b78001edd85a1ba00c3436effd05b8840347
Josh Motta <joshua.a.motta@gmail.com>
2020-11-28 11:22:29 -0500

    init commit

---
+++ CC05_RotateLeft/src/RotateLeftApp.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ * Coding Challenge 5 - Rotate Left
+ * 
+ * Given an array of ints length 3, return an array with the elements 
+ * "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+ * 
+ */
+
+public class RotateLeftApp {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static int[] rotateLeft3(int[] nums) {
+		
+		int[] newArr = nums;
+		
+		return newArr;
+	}
+}
