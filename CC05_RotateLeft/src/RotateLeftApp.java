4a8f664ded13c868573cf735df34ac66a5482ae2
Josh Motta <joshua.a.motta@gmail.com>
2020-11-29 22:42:52 -0500

    Started Logic

--- CC05_RotateLeft/src/RotateLeftApp.java
+++ CC05_RotateLeft/src/RotateLeftApp.java
@@ -11,12 +11,25 @@ public class RotateLeftApp {
 
 	public static void main(String[] args) {
 
+		int[] testArr1 = { 1, 2, 3 };
+		int[] testArr2 = { 5, 11, 9 };
+		int[] testArr3 = { 7, 0, 0 };
+
+		int[] resultsArr = rotateLeft3(testArr1);
+		
+		for(int i = 0; i < resultsArr.length; i++) {
+			System.out.print(resultsArr[i] + " ");
+		}
 	}
-	
+
 	public static int[] rotateLeft3(int[] nums) {
-		
+
 		int[] newArr = nums;
-		
+
+		for (int i = 0; i < nums.length; i++) {
+				newArr[i] = nums[(i+1)];
+		}
+
 		return newArr;
 	}
 }
