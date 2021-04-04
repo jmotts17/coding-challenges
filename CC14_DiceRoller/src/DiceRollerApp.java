9336c1101219250c55f5b52bcc8aea54243db9db
Josh Motta <joshua.a.motta@gmail.com>
2021-04-04 00:05:16 -0400

    Added dice roll logic

--- CC14_DiceRoller/src/DiceRollerApp.java
+++ CC14_DiceRoller/src/DiceRollerApp.java
@@ -1,3 +1,15 @@
+/*
+ * D&D Dice Roller Challenge found on Reddit
+ * 
+ * I love playing D&D with my friends, and my favorite part is creating character sheets. One major part of making character sheets is rolling the character's stats.
+ * Sadly, I have lost all my dice, so I'm asking for your help to make a dice roller for me to use!
+ * The first number is the number of dice to roll, the d just means "dice", it's just used to split up the two numbers, and the second number is how many sides the dice have.
+ * So the above example of "3d6" means "roll 3 6-sided dice". 
+ * The first number, the number of dice to roll, can be any integer between 1 and 100, inclusive.
+ * The second number, the number of sides of the dice, can be any integer between 2 and 100, inclusive.
+ */
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DiceRollerApp {
@@ -13,8 +25,20 @@ public class DiceRollerApp {
 			
 			if(input.contains("d") ) {
 				String[] diceAndSides = input.split("d");
-				System.out.println(diceAndSides[0] + diceAndSides[1]);
-				
+				ArrayList<Integer> rolls = new ArrayList<Integer>();
+				int numOfDice = Integer.parseInt(diceAndSides[0]);
+				int numOfSides = Integer.parseInt(diceAndSides[1]);
+				int total = 0;
+				for(int i=0; i < numOfDice ; i++) {
+					int dieRoll = dieRoll(numOfSides);
+					rolls.add(dieRoll);
+					total += dieRoll;
+				}
+				System.out.print("Total: " + total + " Rolls: ");
+				for(Integer roll : rolls) {
+					System.out.print(roll + " ");
+				}
+				System.out.println();
 			} else if (input.equals("exit")) {
 				break;
 			} else {
@@ -25,7 +49,7 @@ public class DiceRollerApp {
 		System.out.println("Goodbye!");
 	}
 	
-	public static int diceRoll(int s) {
+	public static int dieRoll(int s) {
 		return (int)(Math.random()*s) + 1;
 	}
 
